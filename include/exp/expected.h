1c15dae028d66a3048a5801a394aca1bde2d848e
Simon Truscott <saitora@gmail.com>
2021-09-08 19:07:54 +1000

    Project skeleton

---
+++ include/exp/expected.h
@@ -0,0 +1,18 @@
+#ifndef INCLUDE_EXP_EXPECTED_H
+#define INCLUDE_EXP_EXPECTED_H
+
+namespace exp {
+
+template <class E> class unexpected {
+public:
+private:
+};
+
+template <class T, class E> class expected {
+public:
+private:
+};
+
+} // namespace exp
+
+#endif
