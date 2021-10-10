99f82fd97999218fc7325de8ee2d9850d97b0f1e
Simon Truscott <saitora@gmail.com>
2021-10-11 08:20:14 +1100

    Move test class to its own file

--- test/expected_test.cpp
+++ test/expected_test.cpp
@@ -2,9 +2,9 @@
 
 #include "arg.h"
 #include "obj.h"
+#include "obj_throw.h"
 #include "state.h"
 
-#include <initializer_list>
 #include <utility>
 
 #include <gtest/gtest.h>
@@ -12,95 +12,6 @@
 using namespace exp;
 
 namespace {
-
-enum class May_throw { do_not_throw, do_throw };
-
-struct Val_throw {
-  inline static State s = State::none;
-  static void reset() { s = State::none; }
-
-  explicit Val_throw(int x_) {
-    s = State::constructed;
-    x = x_;
-  }
-
-  Val_throw(Arg&& arg_, int) {
-    s = State::constructed;
-    Arg arg = std::move(arg_);
-    x = arg.x;
-    if (t == May_throw::do_throw)
-      throw t;
-  }
-
-  Val_throw(std::initializer_list<int> il, Arg&& arg_, int) {
-    s = State::constructed;
-    Arg arg = std::move(arg_);
-    x = arg.x;
-    if (!std::empty(il))
-      x += *il.begin();
-    if (t == May_throw::do_throw)
-      throw t;
-  }
-
-  Val_throw(Val_throw&& other) noexcept {
-    s = State::move_constructed;
-    x = other.x;
-    other.x = -1;
-  }
-
-  Val_throw& operator=(Val_throw&& other) noexcept {
-    s = State::move_assigned;
-    x = other.x;
-    other.x = -2;
-    return *this;
-  }
-
-  ~Val_throw() { s = State::destructed; }
-
-  int x = 0;
-  inline static May_throw t = May_throw::do_not_throw;
-};
-
-struct Val_throw_2 {
-  inline static State s = State::none;
-  static void reset() { s = State::none; }
-
-  Val_throw_2(Arg&& arg_, int) {
-    s = State::constructed;
-    Arg arg = std::move(arg_);
-    x = arg.x;
-    if (t == May_throw::do_throw)
-      throw t;
-  }
-
-  Val_throw_2(std::initializer_list<int> il, Arg&& arg_, int) {
-    s = State::constructed;
-    Arg arg = std::move(arg_);
-    x = arg.x;
-    if (!std::empty(il))
-      x += *il.begin();
-    if (t == May_throw::do_throw)
-      throw t;
-  }
-
-  Val_throw_2(Val_throw_2&& other) {
-    s = State::move_constructed;
-    x = other.x;
-    other.x = -1;
-  }
-
-  Val_throw_2& operator=(Val_throw_2&& other) {
-    s = State::move_assigned;
-    x = other.x;
-    other.x = -2;
-    return *this;
-  }
-
-  ~Val_throw_2() { s = State::destructed; }
-
-  int x = 0;
-  inline static May_throw t = May_throw::do_not_throw;
-};
 
 struct Con {
   Con() = default;
