1d858df091a2aff08f379bd155d55b1e4e244899
Simon Truscott <saitora@gmail.com>
2022-03-05 06:31:08 +1100

    Test unexpected constexpr functions

---
+++ test/obj_constexpr.h
@@ -0,0 +1,115 @@
+#ifndef TEST_OBJ_CONSTEXPR_H
+#define TEST_OBJ_CONSTEXPR_H
+
+#include <initializer_list>
+#include <utility>
+
+struct Arg {
+  constexpr explicit Arg(int x_) : x(x_) {}
+
+  Arg(const Arg&) = default;
+
+  constexpr Arg(Arg&& other) noexcept : x(other.x + 201) {}
+
+  Arg& operator=(const Arg&) = default;
+
+  constexpr Arg& operator=(Arg&& other) noexcept {
+    x = other.x + 202;
+    return *this;
+  }
+
+  ~Arg() = default;
+
+  int x;
+};
+
+template <class Tag> struct Obj {
+  constexpr Obj() noexcept : x(20100) {}
+
+  constexpr explicit Obj(int x_) noexcept : x(x_) {}
+
+  constexpr explicit Obj(const Arg& arg_) noexcept : x(arg_.x) {}
+
+  constexpr explicit Obj(Arg&& arg_) noexcept {
+    Arg arg = std::move(arg_);
+    x = arg.x;
+  }
+
+  constexpr Obj(Arg&& arg_, int) noexcept {
+    Arg arg = std::move(arg_);
+    x = arg.x;
+  }
+
+  constexpr Obj(std::initializer_list<int> il, Arg&& arg_, int) noexcept {
+    Arg arg = std::move(arg_);
+    x = arg.x;
+    if (!std::empty(il))
+      x += *il.begin();
+  }
+
+  constexpr Obj(const Obj& other) noexcept : x(other.x) {}
+
+  constexpr Obj(Obj&& other) noexcept : x(other.x + 101) {}
+
+  constexpr Obj& operator=(const Obj& other) noexcept {
+    x = other.x;
+    return *this;
+  }
+
+  constexpr Obj& operator=(Obj&& other) noexcept {
+    x = other.x + 102;
+    return *this;
+  }
+
+  constexpr Obj& operator=(const Arg& arg_) noexcept {
+    x = arg_.x;
+    return *this;
+  }
+
+  constexpr Obj& operator=(Arg&& arg_) noexcept {
+    Arg arg = std::move(arg_);
+    x = arg.x;
+    return *this;
+  }
+
+  ~Obj() = default;
+
+  int x;
+};
+
+template <class Tag1, class Tag2>
+constexpr bool operator==(Obj<Tag1> lhs, Obj<Tag2> rhs) {
+  return lhs.x == rhs.x;
+}
+
+template <class Tag1, class Tag2>
+constexpr bool operator!=(Obj<Tag1> lhs, Obj<Tag2> rhs) {
+  return !(lhs == rhs);
+}
+
+struct Val_tag {};
+using Val = Obj<Val_tag>;
+
+struct Err_tag {};
+using Err = Obj<Err_tag>;
+
+template <class Tag> struct Obj_implicit {
+  constexpr Obj_implicit(int x_) : x(x_) {}
+
+  constexpr Obj_implicit(const Arg& arg_) : x(arg_.x) {}
+
+  constexpr Obj_implicit(Arg&& arg_) {
+    Arg arg = std::move(arg_);
+    x = arg.x;
+  }
+
+  int x;
+};
+
+struct Val_implicit_tag {};
+using Val_implicit = Obj_implicit<Val_implicit_tag>;
+
+struct Err_implicit_tag {};
+using Err_implicit = Obj_implicit<Err_implicit_tag>;
+
+#endif
---
+++ test/unexpected_constexpr_test.cpp
@@ -0,0 +1,227 @@
+#include "bc/exp/expected.h"
+
+#include "obj_constexpr.h"
+
+#include <utility>
+
+#include <gtest/gtest.h>
+
+using namespace bc::exp;
+
+namespace {
+
+constexpr int value_function_const_l_ref(int x) {
+  const unexpected<Err> e(x);
+  const Err& err = e.value();
+  return err.x;
+}
+
+constexpr int value_function_non_const_l_ref(int x) {
+  unexpected<Err> e(x);
+  Err& err = e.value();
+  return err.x;
+}
+
+constexpr int value_function_const_r_ref(int x) {
+  const unexpected<Err> e(x);
+  const Err&& err = std::move(e).value();
+  return err.x;
+}
+
+constexpr int value_function_non_const_r_ref(int x) {
+  unexpected<Err> e(x);
+  Err&& err = std::move(e).value();
+  return err.x;
+}
+
+} // namespace
+
+TEST(unexpected_constexpr, value) {
+  // const& overload
+  {
+    constexpr int x = value_function_const_l_ref(1);
+    ASSERT_EQ(x, 1);
+  }
+  // non-const& overload
+  {
+    constexpr int x = value_function_non_const_l_ref(2);
+    ASSERT_EQ(x, 2);
+  }
+  // const&& overload
+  {
+    constexpr int x = value_function_const_r_ref(3);
+    ASSERT_EQ(x, 3);
+  }
+  // non-const&& overload
+  {
+    constexpr int x = value_function_non_const_r_ref(4);
+    ASSERT_EQ(x, 4);
+  }
+}
+
+namespace {
+
+template <class E> constexpr int copy_constructor(int x) {
+  unexpected<E> other(x);
+  unexpected<Err> e(other);
+  return e.value().x;
+}
+
+template <class E> constexpr int move_constructor(int x) {
+  unexpected<E> other(x);
+  unexpected<Err> e(std::move(other));
+  return e.value().x;
+}
+
+constexpr int implicit_copy_constructor(int x) {
+  unexpected<Arg> other(x);
+  unexpected<Err_implicit> e = other;
+  return e.value().x;
+}
+
+constexpr int implicit_move_constructor(int x) {
+  unexpected<Arg> other(x);
+  unexpected<Err_implicit> e = std::move(other);
+  return e.value().x;
+}
+
+template <class E> constexpr int value_constructor(int x) {
+  E val(x);
+  unexpected<Err> e(std::move(val));
+  return e.value().x;
+}
+
+constexpr int in_place_constructor(int x) {
+  Arg arg(x);
+  unexpected<Err> e(std::in_place, std::move(arg), x);
+  return e.value().x;
+}
+
+constexpr int in_place_initializer_list_constructor(int x) {
+  Arg arg(x);
+  unexpected<Err> e(std::in_place, {x}, std::move(arg), x);
+  return e.value().x;
+}
+
+} // namespace
+
+TEST(unexpected_constexpr, constructors) {
+  // (const unexpected&)
+  {
+    constexpr int x = copy_constructor<Err>(1);
+    ASSERT_EQ(x, 1);
+  }
+  // (unexpected&&)
+  {
+    constexpr int x = move_constructor<Err>(2);
+    ASSERT_EQ(x, 2 + 101);
+  }
+  // explicit (const unexpected<Err>&)
+  {
+    constexpr int x = copy_constructor<Arg>(3);
+    ASSERT_EQ(x, 3);
+  }
+  // implicit (const unexpected<Err>&)
+  {
+    constexpr int x = implicit_copy_constructor(4);
+    ASSERT_EQ(x, 4);
+  }
+  // explicit (unexpected<Err>&&)
+  {
+    constexpr int x = move_constructor<Arg>(5);
+    ASSERT_EQ(x, 5 + 201);
+  }
+  // implicit (unexpected<Err>&&)
+  {
+    constexpr int x = implicit_move_constructor(6);
+    ASSERT_EQ(x, 6 + 201);
+  }
+  // (Err&&) with Err = E
+  {
+    constexpr int x = value_constructor<Err>(7);
+    ASSERT_EQ(x, 7 + 101);
+  }
+  // (Err&&) with Err != E
+  {
+    constexpr int x = value_constructor<Arg>(8);
+    ASSERT_EQ(x, 8 + 201);
+  }
+  // (std::in_place_t, Args&&...)
+  {
+    constexpr int x = in_place_constructor(9);
+    ASSERT_EQ(x, 9 + 201);
+  }
+  // (std::in_place_t, std::initializer_list<U>, Args&&...)
+  {
+    constexpr int x = in_place_initializer_list_constructor(10);
+    ASSERT_EQ(x, 10 + 10 + 201);
+  }
+}
+
+namespace {
+
+template <class E> constexpr int copy_assignment(int x) {
+  unexpected<E> other(x);
+  unexpected<Err> e(10 * x);
+  e = other;
+  return e.value().x;
+}
+
+template <class E> constexpr int move_assignment(int x) {
+  unexpected<E> other(x);
+  unexpected<Err> e(10 * x);
+  e = std::move(other);
+  return e.value().x;
+}
+
+} // namespace
+
+TEST(unexpected_constexpr, assignment_operators) {
+  // (const unexpected&)
+  {
+    constexpr int x = copy_assignment<Err>(1);
+    ASSERT_EQ(x, 1);
+  }
+  // (unexpected&&)
+  {
+    constexpr int x = move_assignment<Err>(2);
+    ASSERT_EQ(x, 2 + 102);
+  }
+  // (const unexpected<Err>&)
+  {
+    constexpr int x = copy_assignment<Arg>(3);
+    ASSERT_EQ(x, 3);
+  }
+  // (unexpected<Err>&&)
+  {
+    constexpr int x = move_assignment<Arg>(4);
+    ASSERT_EQ(x, 4 + 201);
+  }
+}
+
+namespace {
+
+constexpr bool equal_to_operator(int x, int y) {
+  unexpected<Err> e1(x);
+  unexpected<Err> e2(y);
+  return e1 == e2;
+}
+
+constexpr bool not_equal_to_operator(int x, int y) {
+  unexpected<Err> e1(x);
+  unexpected<Err> e2(y);
+  return e1 != e2;
+}
+
+} // namespace
+
+TEST(unexpected_constexpr, equality_operators) {
+  {
+    constexpr bool b = equal_to_operator(1, 1);
+    ASSERT_TRUE(b);
+  }
+  {
+    constexpr bool b = not_equal_to_operator(2, 2);
+    ASSERT_FALSE(b);
+  }
+}
